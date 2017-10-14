9a75d8ccea801a61b0be9ed2e5610a1166ecfe55
root <root@vthor-27.fritz.box>
2017-10-14 16:35:24 +0200

    resolve bind mounts

--- test/test1.py
+++ test/test1.py
@@ -117,8 +117,6 @@ class TestStringMethods(unittest.TestCase):
 
 
 
-
-
 if __name__ == '__main__':
     #unittest.main()
     suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
